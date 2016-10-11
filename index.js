f4a51f3023f1e015464e0832d7d6ea0d014d2309
feichao <len.may@foxmail.com>
2016-10-11 22:51:33 +0800

    add style

--- index.js
+++ index.js
@@ -13,17 +13,28 @@ window.InterpeterMobile = window.InterpeterMobile || {
 		var self = this;
 		document.addEventListener('keydown', function(event) {
 			if(event.key === 'Enter' || event.keyCode === 13) {
-				self.appendToHistory(self.executeStatement());
-				self.clearStatement();
+				event.preventDefault();
+				setTimeout(function() {
+					self.appendToHistory(self.executeStatement());
+					self.clearStatement();
+				}, 0);
 			} else {
 				self.resizeInputElement();
 			}
 		});
 	},
 	executeStatement: function() {
+		var result;
+
+		try {
+			result = eval.call(window, this.inputStatementElement.value);
+		} catch(exception) {
+			result = exception.message;
+		}
+
 		return {
 			statement: this.inputStatementElement.value,
-			executeResult: eval(this.inputStatementElement.value)
+			executeResult: result
 		};
 	},
 	resizeInputElement: function() {
@@ -44,13 +55,17 @@ window.InterpeterMobile = window.InterpeterMobile || {
 		return pre;
 	},
 	appendToHistory: function(result) {
+		var historyWarper = document.createElement('li');
+
 		var nodeStatement = this.createHostoryElement('statement');
 		nodeStatement.children[0].innerText = result.statement;
-		this.historyStatementWraper.appendChild(nodeStatement);
+		historyWarper.appendChild(nodeStatement);
 
 		var nodeExecuteResult = this.createHostoryElement('execute-result');
 		nodeExecuteResult.children[0].innerText = result.executeResult;
-		this.historyStatementWraper.appendChild(nodeExecuteResult);
+		historyWarper.appendChild(nodeExecuteResult);
+
+		this.historyStatementWraper.appendChild(historyWarper);
 	},
 };
 
