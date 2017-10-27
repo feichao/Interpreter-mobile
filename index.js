dd005cdd1c8a1d4296c85ba5e80cbaf7680e8961
wanfeichao <wanfeichao@cvte.com>
2017-10-27 19:36:11 +0800

    support https

--- index.js
+++ index.js
@@ -135,12 +135,12 @@ window.InterpeterLibsStorage = function() {
 
 	if(this.libs.length <= 0) {
 		this.libs = [{
-			name: 'jQuery 2.2.1',
-			url: 'http://7xlphe.com1.z0.glb.clouddn.com/jquery.min.js',
+			name: 'jQuery 3.2.1',
+			url: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js',
 			isLoad: false,
 		}, {
 			name: 'Babel for ES6',
-			url: 'http://7xlphe.com1.z0.glb.clouddn.com/babel.min.js',
+			url: 'https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.26.0/babel.min.js',
 			isLoad: false,
 		}];
 	}
